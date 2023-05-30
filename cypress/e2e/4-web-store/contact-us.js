006cded1621480d6513e294eb0eadfe5b1e081ee
andreinalivaika <nav.testsw@gmail.com>
2023-05-30 14:00:42 +0300

    02 add web store contact us test

--- cypress/e2e/4-web-store/contact-us.js
+++ cypress/e2e/4-web-store/contact-us.js
@@ -1,6 +1,16 @@
 describe("Test Contact Us form via web store", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
-
+        cy.visit('https://www.automationteststore.com/')
+        cy.get('a[href="https://automationteststore.com/index.php?rt=content/contact"]').click()
+        cy.get('#ContactUsFrm_first_name').type("aaa")
+        cy.get('#ContactUsFrm_email').type("aaa@gmail.com")
+        cy.get('//textarea[@id="ContactUsFrm_enquiry"]').type("test comment")
+        cy.get('button[title="Submit"]').click().then(() => {
+            cy.get('.mb40 > :nth-child(3)').should('contain', 'Your enquiry has been successfully sent to the store owner!')
+            cy.location().should((loc) => {
+                expect(loc.search).to.equal("?rt=content/contact/success")
+            })
+        })
     })
 
 })
