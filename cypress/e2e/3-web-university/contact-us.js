4088afcf052b0e6eb31a3cd8b0bf16431e2402b6
andreinalivaika <nav.testsw@gmail.com>
2023-06-01 17:45:52 +0300

    09 add handle redirect to new browser tab test

--- cypress/e2e/3-web-university/contact-us.js
+++ cypress/e2e/3-web-university/contact-us.js
@@ -19,5 +19,17 @@ describe("Test Contact Us form via WebdriverUniversity", () => {
             cy.get('body').should('contain', 'Error: all fields are required')
         })
     })
+
+    it.only("Should be able open form on the same browser tab and submit a successful submission via contact us form", () => {
+        cy.visit("https://www.webdriveruniversity.com")
+        cy.get('#contact-us').invoke('removeAttr', 'target').click()
+        cy.get('[name="first_name"]').type("aaa")
+        cy.get('[name="last_name"]').type("bbb")
+        cy.get('[name="email"]').type("aaa@gmail.com")
+        cy.get('textarea.feedback-input').type("test comment")
+        cy.get('[type="submit"]').click()
+        cy.get('#contact_reply > h1').should("have.text", "Thank You for your Message!")
+        cy.url().should("include", "contact-form-thank-you")
+    })
 })
 
