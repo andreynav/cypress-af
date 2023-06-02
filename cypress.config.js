3f8b4a369c4134d8630e5b4ecc80d31d249b8ebb
andreinalivaika <nav.testsw@gmail.com>
2023-06-02 14:51:46 +0300

    10 add browser navigation test

--- cypress.config.js
+++ cypress.config.js
@@ -7,5 +7,8 @@ module.exports = defineConfig({
     },
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
     defaultCommandTimeout: 7000,
+    chromeWebSecurity: false,
+    viewportWidth: 1000,
+    viewportHeight: 800,
   },
 });
