5b8cb41fc7f5b176c46afcd90c5630be8bcbc4b1
andreinalivaika <nav.testsw@gmail.com>
2023-06-05 14:20:29 +0300

    23 add fixtures for existing tests

--- cypress/e2e/03-web-university/contact-us.js
+++ cypress/e2e/03-web-university/contact-us.js
@@ -1,35 +1,36 @@
 describe("Test Contact Us form via WebdriverUniversity", () => {
-    it("Should be able to submit a successful submission via contact us form", () => {
-        cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
-        cy.get('[name="first_name"]').type("aaa")
-        cy.get('[name="last_name"]').type("bbb")
-        cy.get('[name="email"]').type("aaa@gmail.com")
-        cy.get('textarea.feedback-input').type("test comment")
-        cy.get('[type="submit"]').click()
-        cy.get('#contact_reply > h1').should("have.text", "Thank You for your Message!")
-        cy.url().should("include", "contact-form-thank-you")
-    })
-
-    it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-        cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
-        cy.get('[name="first_name"]').type("aaa")
-        cy.get('[name="last_name"]').type("bbb")
-        cy.get('[name="email"]').type("aaa@gmail.com")
-        cy.get('[type="submit"]').click().then(() => {
-            cy.get('body').should('contain', 'Error: all fields are required')
+    before(function() {
+        /**
+         * @param {{first_name:string, last_name:string, email:string, body:string, success_response:string,
+         * success_url:string, error_response:string}} data
+         */
+        return cy.fixture('contact-us.json').then((data) => {
+            this.data = data
         })
     })
 
-    it.only("Should be able open form on the same browser tab and submit a successful submission via contact us form", () => {
+    beforeEach(() => {
         cy.visit("https://www.webdriveruniversity.com")
         cy.openPageInCurrentTab('#contact-us')
-        cy.get('[name="first_name"]').type("aaa")
-        cy.get('[name="last_name"]').type("bbb")
-        cy.get('[name="email"]').type("aaa@gmail.com")
-        cy.get('textarea.feedback-input').type("test comment")
+    })
+
+    it("Should not be able to submit a successful submission via contact us form as all fields are required", function() {
+        cy.get('[name="first_name"]').type(this.data.first_name)
+        cy.get('[name="last_name"]').type(this.data.last_name)
+        cy.get('[name="email"]').type(this.data.email)
+        cy.get('[type="submit"]').click().then(() => {
+            cy.get('body').should('contain', this.data.error_response)
+        })
+    })
+
+    it("Should be able open form on the same browser tab and submit a successful submission via contact us form", function() {
+        cy.get('[name="first_name"]').type(this.data.first_name)
+        cy.get('[name="last_name"]').type(this.data.last_name)
+        cy.get('[name="email"]').type(this.data.email)
+        cy.get('textarea.feedback-input').type(this.data.body)
         cy.get('[type="submit"]').click()
-        cy.get('#contact_reply > h1').should("have.text", "Thank You for your Message!")
-        cy.url().should("include", "contact-form-thank-you")
+        cy.get('#contact_reply > h1').should("have.text", this.data.success_response)
+        cy.url().should("include", this.data.success_url)
     })
 })
 
